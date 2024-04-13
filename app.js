2b476e3d4d22d3cbddae202ed746afd505aea560
mirajsankdecha <mirajsankdecha@gmail.com>
2024-04-13 18:47:23 +0530

    git init -y

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Hii API is Live!");
+});
+const start = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
