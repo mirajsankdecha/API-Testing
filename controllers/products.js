86357a34868d010dccfad11f8a31b94139d7c7bf
mirajsankdecha <mirajsankdecha@gmail.com>
2024-04-14 19:26:39 +0530

    products added in json

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,7 @@
+const Product = require("../models/products");
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ message: "Get All Products" });
+  const Data = await Product.find({});
+  res.status(200).json({ Data });
 };
 
 const getAllProductsTesting = async (req, res) => {
