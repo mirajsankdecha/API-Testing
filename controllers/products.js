f2852e754841fb303d3965beda3af266bf42322e
mirajsankdecha <mirajsankdecha@gmail.com>
2024-04-16 22:57:45 +0530

    Filter by company functionality added in controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,11 @@
 const Product = require("../models/products");
 const getAllProducts = async (req, res) => {
-  const Data = await Product.find({});
+  const { company } = req.query;
+  const queryObject = {};
+  if (company) {
+    queryObject.company = company;
+  }
+  const Data = await Product.find(queryObject);
   res.status(200).json({ Data });
 };
 
