ac65655922140a9411d6e709e0694c2ea308abcf
mirajsankdecha <mirajsankdecha@gmail.com>
2024-04-16 23:11:11 +0530

    Filter by featured functionality added in controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,15 @@
 const Product = require("../models/products");
 const getAllProducts = async (req, res) => {
-  const { company , name } = req.query;
+  const { company , name , featured } = req.query;
   const queryObject = {};
   if (company) {
     queryObject.company = company;
   }
   if (name) {
     queryObject.name = { $regex: name, $options: "i"}; 
+  }
+  if (featured) {
+    queryObject.featured = featured;
   }
   const Data = await Product.find(queryObject);
   res.status(200).json({ Data });
