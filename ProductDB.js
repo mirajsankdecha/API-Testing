0934b5bd5da13f75f1d880bc2259135b74c75e8a
mirajsankdecha <mirajsankdecha@gmail.com>
2024-04-16 22:47:20 +0530

    Delete functionality added

--- ProductDB.js
+++ ProductDB.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const connectDB = require("./db/connect");
+const products = require("./models/products");
 const Products = require("./models/products");
 const Product_JSON = require("./products.json");
 const start = async () => {
     try {
         await connectDB(process.env.MONGODB_URL);
+        await products.deleteMany();
         await Products.create(Product_JSON);
         console.log("Data Inserted Successfully");
     } catch (error) {
